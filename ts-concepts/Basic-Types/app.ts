73b8738cd2a17e04c850a42206f8b395e111b006
youshamahmood <youshamahmood96@gmail.com>
2022-02-09 17:13:02 +0600

    type assignment & type inference

--- ts-concepts/Basic-Types/app.ts
+++ ts-concepts/Basic-Types/app.ts
@@ -1,4 +1,10 @@
-const num1:number = 5;
-const num2:number = 7;
-console.log(num1+num2);
+const num1:number = 5; // Type assignment
+const num2:number = 7; // no difference with 5.0
+const num3 = 8; // Not using :number here. Typescript uses type inference here.
+let num4:number;
+num4 = 5; // When we use this type of assignment, its wiser to mention the type of the variable.
 
+function add(a:number,b:number):number{
+    return a+b;
+}
+console.log(add(num1,num2));
